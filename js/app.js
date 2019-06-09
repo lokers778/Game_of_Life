251d67affb2aa75fbbc00384ce6766b7d90a3bcf
lokers778 <lokers778@gmail.com>
2019-06-09 09:34:55 +0200

    add class toogle

--- js/app.js
+++ js/app.js
@@ -8,19 +8,32 @@ document.addEventListener("DOMContentLoaded", function () {
             this.board = GameBoard,
             this.cells = [],
             this.createBoard = function () {
-                this.board.style.width = this.width*10+"px";
-                this.board.style.height = this.height*10+"px";
-                var allPlace =boardHeight*boardWidth;
-                for(var i=0;i<allPlace;i++){
-            var cell=document.createElement("div");
-                    this.board.appendChild(cell)
+                this.board.style.width = this.width * 10 + "px";
+                this.board.style.height = this.height * 10 + "px";
+                var allPlace = boardHeight * boardWidth;
+                for (var i = 0; i < allPlace; i++) {
+                    var cell = document.createElement("div");
+                    this.cells.push(cell)
+                    this.board.appendChild(cell);
                 }
-        }
+                this.cells.forEach(function (target) {
+                    target.addEventListener("click", function () {
+                       target.classList.toggle("live");
+                        console.log("hello")
+                    })
+                })
+            }
+            this.calculateNeighbour=function (x,y) {
+
+                
+            }
     }
 
 
     var game = new GameOfLife(50, 50);
     game.createBoard();
+    console.log(game.cells)
 
 
-});
+})
+;
